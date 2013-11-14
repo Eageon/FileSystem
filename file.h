ea0a8b316c73bb2bc753cd10f8e86dd39216ba5c
Blizzardking <renkaikevin.ji@gmail.com>
2013-11-13 23:44:32 -0600

    1113 night in dallas

--- file.h
+++ file.h
@@ -18,6 +18,7 @@ struct V6_file {
 int read_file_by_inode(struct inode *file_inode, byte* buf);
 int write_file_by_inode(struct inode *file_inode, byte* buf, size_t count);
 int read_directory(struct inode *dir_inode, struct file_entry **entries, int *entry_num);
+
 uint find_file_in_directory(const char *filename, struct V6_file *spec_dir);
 uint find_file_in_current_directory(const char *filename)
 

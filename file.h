0e5c627d79c05ef41f09a5536145ec2f736b3f35
Eageon <yujun.eageon@gmail.com>
2013-11-13 22:26:26 -0600

    API file ok

--- file.h
+++ file.h
@@ -13,10 +13,14 @@ struct V6_file {
 #define file_entry V6_file
 
 
-#define FILE_ENTRY_SIZE sizeof(struct V6_file)
+#define FILE_ENTRY_SIZE (sizeof(struct V6_file))
 
-extern ssize_t read_file(const char *filename, void *buf, size_t count);
-extern ssize_t write_file(char *filename, const void *buf, size_t count);
+int read_file_by_inode(struct inode *file_inode, byte* buf);
+int write_file_by_inode(struct inode *file_inode, byte* buf, size_t count);
+int read_directory(struct inode *dir_inode, struct file_entry **entries, int *entry_num);
+uint find_file_in_directory(const char *filename, struct V6_file *spec_dir);
+uint find_file_in_current_directory(const char *filename)
+
 //int current_directory(const char *filename);
 //extern int read_directory(struct inode *dir_inode, struct file_entry *entries, int *entry_num);
 
